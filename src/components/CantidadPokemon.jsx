0609e976bf7803efbd92dd1d9728db75586c7df1
jhhg04 <jhhg04@gmail.com>
2021-09-29 23:02:09 -0500

    first commit

---
+++ src/components/CantidadPokemon.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class CantidadPokemon extends Component {
+  render() {
+    return <>Unidades : 30</>;
+  }
+}
+
+export default CantidadPokemon;
---
+++ src/components/CompraPokemon.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export class CompraPokemon extends Component {
+  render() {
+    return (
+      <>
+        <button className="btn btn-dark btn-sm mb-2">Comprar Pokemon</button>
+        <button className="btn btn-dark btn-sm">Regresar Pokemon</button>
+      </>
+    );
+  }
+}
+
+export default CompraPokemon;
